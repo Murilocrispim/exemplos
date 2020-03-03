2ffdf8654dbf1ac7786efae2fcb85ab6156f7f08
Murilocrispim <crispimmurilo@hotmail.com>
2020-03-03 17:45:51 -0300

    exemplo3a

---
+++ 20200303/exemplo3.js
@@ -0,0 +1,10 @@
+function escreve_mensagem(valor){
+    mensagem = "Você escreveu '"+valor+"' ";
+    alert(mensagem);
+    if(isNaN(valor)){
+        alert(valor +" é um número");
+    }
+    else{
+        alert(valor +" é um numero");
+    }
+}
